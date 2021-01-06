754128c22d120be0e051f8df18abfdf7a93a7bff
Giando <giando.olini@gogodigital.it>
2021-01-06 20:57:34 +0100

    Update SNS

--- models/SNS.php
+++ models/SNS.php
@@ -183,4 +183,167 @@ class SNS extends Model
 		/** @var Result $result */
 		return $result;
 	}
+
+	/**
+	 * Subscribe an Email Address to a Topic
+	 *
+	 * To initiate a subscription to an email address, use the Subscribe operation.
+	 * You can use the subscribe method to subscribe several different endpoints to an Amazon SNS topic,
+	 * depending on the values used for parameters passed.
+	 * This is shown in other examples in this topic.
+	 *
+	 * @param string $protocol
+	 * @param string $endpoint
+	 * @param string $topic
+	 *
+	 * @example $protocol = 'email';
+	 * @example $endpoint = 'sample@example.com';
+	 * @example $topic = 'arn:aws:sns:us-east-1:111122223333:MyTopic';
+	 */
+	public function subscribeEmailToTopic($protocol = 'email', $endpoint, $topic)
+	{
+		try {
+			$result = $SnSclient->subscribe([
+				'Protocol' => $protocol,
+				'Endpoint' => $endpoint,
+				'ReturnSubscriptionArn' => true,
+				'TopicArn' => $topic,
+			]);
+			Yii::$app->session->setFlash('success', Yii::t('aws', 'Email {0} subscribed to Topic correctly', $endpoint));
+		} catch (AwsException $e) {
+			Yii::$app->session->setFlash('error', $e->getMessage());
+		}
+
+		/** @var Result $result */
+		return $result;
+	}
+
+	/**
+	 * Subscribe an Application Endpoint to a Topic
+	 *
+	 * To initiate a subscription to a web app, use the Subscribe operation.
+	 * You can use the subscribe method to subscribe several different endpoints to an Amazon SNS topic,
+	 * depending on the values used for parameters passed. This is shown in other examples in this topic.
+	 *
+	 * @param string $protocol
+	 * @param string $endpoint
+	 * @param string $topic
+	 *
+	 * @example $protocol = 'https';
+	 * @example $endpoint = 'https://';
+	 * @example $topic = 'arn:aws:sns:us-east-1:111122223333:MyTopic';
+	 */
+	public function subscribeAppEndPointToTopic($protocol = 'https', $endpoint, $topic)
+	{
+		try {
+			$result = $SnSclient->subscribe([
+				'Protocol' => $protocol,
+				'Endpoint' => $endpoint,
+				'ReturnSubscriptionArn' => true,
+				'TopicArn' => $topic,
+			]);
+			Yii::$app->session->setFlash('success', Yii::t('aws', 'Email {0} subscribed to Topic correctly', $endpoint));
+		} catch (AwsException $e) {
+			Yii::$app->session->setFlash('error', $e->getMessage());
+		}
+
+		/** @var Result $result */
+		return $result;
+	}
+
+	/**
+	 * Subscribe a Lambda Function to a Topic
+	 *
+	 * To initiate a subscription to a Lambda function, use the Subscribe operation.
+	 * You can use the subscribe method to subscribe several different endpoints to an Amazon SNS topic,
+	 * depending on the values used for parameters passed. This is shown in other examples in this topic.
+	 *
+	 * @param string $protocol
+	 * @param string $endpoint
+	 * @param string $topic
+	 *
+	 * @example $protocol = 'lambda';
+	 * @example $endpoint = 'arn:aws:lambda:us-east-1:123456789023:function:messageStore';
+	 * @example $topic = 'arn:aws:sns:us-east-1:111122223333:MyTopic';
+	 */
+	public function subscribeLambdaFunctionToTopic($protocol = 'lambda', $endpoint, $topic)
+	{
+		try {
+			$result = $SnSclient->subscribe([
+				'Protocol' => $protocol,
+				'Endpoint' => $endpoint,
+				'ReturnSubscriptionArn' => true,
+				'TopicArn' => $topic,
+			]);
+			Yii::$app->session->setFlash('success', Yii::t('aws', 'Email {0} subscribed to Topic correctly', $endpoint));
+		} catch (AwsException $e) {
+			Yii::$app->session->setFlash('error', $e->getMessage());
+		}
+
+		/** @var Result $result */
+		return $result;
+	}
+
+	/**
+	 * Subscribe a Text SMS to a Topic
+	 *
+	 * To send SMS messages to multiple phone numbers at the same time, subscribe each number to a topic.
+	 * To initiate a subscription to a phone number, use the Subscribe operation.
+	 * You can use the subscribe method to subscribe several different endpoints to an Amazon SNS topic,
+	 * depending on the values used for parameters passed. This is shown in other examples in this topic.
+	 *
+	 * @param string $protocol
+	 * @param string $endpoint
+	 * @param string $topic
+	 *
+	 * @example $protocol = 'sms';
+	 * @example $endpoint = '+1XXX5550100';
+	 * @example $topic = 'arn:aws:sns:us-east-1:111122223333:MyTopic';
+	 */
+	public function subscribeTextSMSToTopic($protocol = 'sms', $endpoint, $topic)
+	{
+		try {
+			$result = $SnSclient->subscribe([
+				'Protocol' => $protocol,
+				'Endpoint' => $endpoint,
+				'ReturnSubscriptionArn' => true,
+				'TopicArn' => $topic,
+			]);
+			Yii::$app->session->setFlash('success', Yii::t('aws', 'Email {0} subscribed to Topic correctly', $endpoint));
+		} catch (AwsException $e) {
+			Yii::$app->session->setFlash('error', $e->getMessage());
+		}
+
+		/** @var Result $result */
+		return $result;
+	}
+
+	/**
+	 * Confirm Subscription to a Topic
+	 *
+	 * To actually create a subscription, the endpoint owner must acknowledge intent to receive messages
+	 * from the topic using a token sent when a subscription is established initially, as described earlier.
+	 * Confirmation tokens are valid for three days. After three days, you can resend a token by creating a new subscription.
+	 *
+	 * @param string $subscription_token
+	 * @param string $topic
+	 *
+	 * @example $subscription_token = 'arn:aws:sns:us-east-1:111122223333:MyTopic:123456-abcd-12ab-1234-12ba3dc1234a';
+	 * @example $topic = 'arn:aws:sns:us-east-1:111122223333:MyTopic';
+	 */
+	public function confirmSubscriptionToTopic($subscription_token, $topic)
+	{
+		try {
+			$result = $SnSclient->subscribe([
+				'Token' => $subscription_token,
+				'TopicArn' => $topic,
+			]);
+			Yii::$app->session->setFlash('success', Yii::t('aws', 'Email {0} subscribed to Topic correctly', $endpoint));
+		} catch (AwsException $e) {
+			Yii::$app->session->setFlash('error', $e->getMessage());
+		}
+
+		/** @var Result $result */
+		return $result;
+	}
 }
